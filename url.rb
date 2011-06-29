38f52b0da593ad909c4c3ee433f828e4b740390c
dljeong <jdl123123@naver.com>
2011-06-29 10:18:14 -0700

    Completed Program, --Open Url directed by each lines, Read Content

--- url.rb
+++ url.rb
@@ -3,7 +3,10 @@ require 'open-uri'
 open("http://log.kaist.ac.kr/ESK/ruby-1/urls.txt") do |f|
 #	puts f.read
 	f.readlines.each do |line|
-		puts line
+#		puts line
+		open(line) do |of|
+			puts of.read
+		end
 	end
 end
 
